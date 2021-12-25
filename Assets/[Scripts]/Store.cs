2c905daa4c6a6f8320ca6aaf12bd2217802ae5af
AnDevX <andil@andevx.com>
2021-12-25 20:08:40 +0000

    [Autorotation & Anchors]
    Configured anchors on icons on all the scenes

--- Assets/[Scripts]/Store.cs
+++ Assets/[Scripts]/Store.cs
@@ -6,13 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class Store : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) 
